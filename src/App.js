12b8ebe102865cba3dcc969fd2dc270032761ced
popyson1648 <popyson1648@gmail.com>
2025-07-10 20:27:59 +0900

    refactor: render board using nested loops instead of hardcoded squares

--- src/App.js
+++ src/App.js
@@ -32,26 +32,21 @@ function Board({ xIsNext, squares, onPlay }) {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
-  return (
-  <>
-    <div className="status">{status}</div>
+  let squareCounter = 0;
 
-    <div className="board-row">
-      <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-      <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-      <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-    </div>
-    <div className="board-row">
-      <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-      <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-      <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-    </div>
-    <div className="board-row">
-      <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-      <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-      <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-    </div>
-  </>
+  return (
+    <>
+      <div className="status">{status}</div>
+
+      {Array.from({ length:3 }).map((_, i) => (
+        <div key={i} className="board-row">
+          {Array.from({ length:3 }).map((_, j) => {
+            const index = i*3+j;
+            return ( <Square key={j} value={squares[index]} onSquareClick={() => handleClick(index)} />);
+          })}
+        </div>
+      ))}
+   </>
   );
 }
 
